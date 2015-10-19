c69cb210a724e622403e8236beaf5fe3977f1ce5
Leou <wly314>
2015-10-19 16:29:07 +0800

    Initial Commit

---
+++ TestXMPPDesktop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestXMPPDesktop
+//
+//  Created by LeouW on 15/10/19.
+//  Copyright © 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
